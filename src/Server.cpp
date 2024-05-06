763882578bcbf95861580bc94aa3c779106b30f5
aryan011 <guptaaryan380@gmail.com>
2024-05-06 18:08:35 +0530

    3rd task

--- src/Server.cpp
+++ src/Server.cpp
@@ -48,36 +48,37 @@ int main(int argc, char **argv) {
   int client_addr_len = sizeof(client_addr);
   
   cout << "Waiting for a client to connect...\n";
-  
-  int client_sock = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
-  if (client_sock < 0) {
-      cerr << "Failed to accept connection\n";
+  for(int i=0;i<2;i++){
+
+    int client_sock = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+    if (client_sock < 0) {
+        cerr << "Failed to accept connection\n";
+    }
+
+    cout << "Client connected\n";
+
+    // Receiving ping request
+    char buffer[1024];
+    int bytes_recvd = recv(client_sock, buffer, sizeof(buffer), 0);
+
+    if (bytes_recvd < 0) {
+        cerr << "Error in receiving data\n";
+        close(client_sock);
+    } else if (bytes_recvd == 0) {
+        cout << "Client disconnected\n";
+        close(client_sock);
+    } else {
+        cout << "Received: " << buffer << endl;
+        // Sending pong 
+        string response = "+PONG\r\n";
+        send(client_sock, response.c_str(), response.length(), 0);
+    }
+
+    // Close the client socket after communication
+    close(client_sock);
   }
-
-cout << "Client connected\n";
-
-// Receiving ping request
-char buffer[1024];
-int bytes_recvd = recv(client_sock, buffer, sizeof(buffer), 0);
-
-if (bytes_recvd < 0) {
-    cerr << "Error in receiving data\n";
-    close(client_sock);
-} else if (bytes_recvd == 0) {
-    cout << "Client disconnected\n";
-    close(client_sock);
-} else {
-    cout << "Received: " << buffer << endl;
-    // Sending pong 
-    string response = "+PONG\r\n";
-    send(client_sock, response.c_str(), response.length(), 0);
-}
-
-// Close the client socket after communication
-close(client_sock);
-
-// Close the server socket after serving the client
-close(server_fd);
+  // Close the server socket after serving the client
+  close(server_fd);
 
   return 0;
 }
