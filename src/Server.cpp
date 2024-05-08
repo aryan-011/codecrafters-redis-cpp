5dbbdec2c2c75b30dd07330fac93e5640514a051
aryan011 <guptaaryan380@gmail.com>
2024-05-09 01:57:56 +0530

    t17

--- src/Server.cpp
+++ src/Server.cpp
@@ -123,7 +123,7 @@ void handleClient(int client_sock)
 
         std::string rdb = "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2";
         std::string res = hexStringToBytes(rdb);
-        response = "$"+res.length()+"\r\n"+res;
+        response = "$"+to_string(res.length())+"\r\n"+res;
         send(client_sock, response.c_str(), response.length(), 0);
       }
     }
