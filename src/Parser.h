44c34152cd681947cb458fc8c03073dc6d2b6f55
aryan011 <guptaaryan380@gmail.com>
2024-05-23 10:34:15 +0530

    t:20:f12 multiple commands

--- src/Parser.h
+++ src/Parser.h
@@ -5,7 +5,7 @@
 #include <string>
 
 char upper(char c);
-std::vector<std::string> parseResp(char *buffer);
+std::vector<std::vector<std::string>> parseResp(char *buffer);
 std::vector<std::string> parseSimpleString(const std::string& raw_message);
 std::vector<std::string> parseBulkString(const std::string& raw_message);
 std::vector<std::string> parseArray(const std::string& raw_message);
